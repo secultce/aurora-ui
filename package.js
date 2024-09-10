21571b3ffefd7d395702bf3ca9c3609ec155a993
TalysonSoares <talysonsoares40@gmail.com>
2024-09-10 11:21:06 -0300

    Update package.js to include new CSS files for fixing layout issues

--- package.js
+++ package.js
@@ -12,8 +12,20 @@ Package.describe({
 Package.onUse(api => {
   api.versionsFrom('METEOR@1.0')
   api.addFiles([
+    'dist/css/accordion.css',
     'dist/css/bootstrap.css',
     'dist/js/bootstrap.js',
-    'dist/css/custom.css'
+    'dist/css/card.css',
+    'dist/css/color.css',
+    'dist/css/custom.css',
+    'dist/css/editor.css',
+    'dist/css/faq.css',
+    'dist/css/forms.css',
+    'dist/js/form-stepper.js',
+    'dist/css/layout.css',
+    'dist/css/navigation.css',
+    'dist/css/side-filters.css',
+    'dist/js/side-filters.js',
+    'dist/css/timeline.css',
   ], 'client')
 })
