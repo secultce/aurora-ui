4b57060db4ae5d03b8ac50b2c8e998632f576491
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2024-09-22 16:16:05 -0300

    Update version to 5.3.13

--- package.js
+++ package.js
@@ -5,7 +5,7 @@
 Package.describe({
   name: 'alessandrofeitoza:aurora-user-interface', // https://atmospherejs.com/twbs/bootstrap
   summary: 'Based on the most popular front-end framework: Bootstrap.',
-  version: '5.3.12',
+  version: '5.3.13',
   git: 'https://github.com/secultce/aurora-ui.git'
 })
 
